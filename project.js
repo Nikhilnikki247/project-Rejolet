29d27d68b7234433fa29ef471e236ac88eb45eae
Nikhilnikki247 <75871420+Nikhilnikki247@users.noreply.github.com>
2021-05-06 12:01:59 +0530

    modified js

--- project.js
+++ project.js
@@ -1,1 +1,7 @@
-write your  js  code here
+let startingPage = document.getElementById("startingPage");
+let crossBtn = document.getElementById("crossBtn");
+let clickContainer = document.getElementById("clickContainer");
+
+crossBtn.addEventListener("click", function() {
+    startingPage.removeChild(clickContainer);
+})
